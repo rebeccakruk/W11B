5ecb1e9f8e824f97f5b75a2c124812e98890ca51
rk <rebeccakruk@outlook.com>
2022-11-21 16:58:33 -0700

    working on object error

--- js/app.js
+++ js/app.js
@@ -1,81 +1,108 @@
+let itemsArray = [
+    {
+        id: 1,
+        imageUrl: "https://cb.scene7.com/is/image/Crate/GoldWhiteOpticBallOrnSSF22?$web_pdp_main_carousel_thumb_med$", 
+        description: `White and Gold Optic Ball Ornament`, 
+        price: 5.0
+    },
+    {
+        id: 2,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: 9.0,
+    },
+    {
+        id: 3,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: 6.0,
+    },
+    {
+        id: 4,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: 8.0,
+    },
+    {
+        id: 5,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: 7.0,
+    },
+    {
+        id: 6,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/NordicGnomeSSF20?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: 10.0,
+    },
+];
+console.log(itemsArray);
+
+itemsArray.map((item) => {
+    document.getElementById(`items`).insertAdjacentHTML(`afterbegin`, 
+    `<div id=${item.id}>
+        <h2>${item.description}</h2>
+        <img src=${item.imageUrl} alt=${item.description}>
+        <p class="price">${item.price}</p>
+        <button onClick="addToCart(${item})">Add To Cart</button>
+    </div>
+    `);
+})
+
+// let cart = [];
+
+function addToCart(){
+// cart.push(item);
+console.log(`clicked`);
+}
+
 // // Cookies.set(`AddedToCart`)
 // items = {};
-function appendNode(parent, element){
-    parent.appendChild(element);
-};
+// function appendNode(parent, element){
+//     parent.appendChild(element);
+// };
 
-function getDiv(container){
-    return document.getElementById(container);
-};
+// function addToCart(action){
+//     let container = '';
+//     if(action == "add") {
+//         container = getDiv("items");
+//         takeAction(container)
+//     } else if (action == "remove") {
+//         container = getDiv("cart");
+//         takeAction(container);
 
-function createNote(node){
-    let element = document.createdElement(node);
-    return element;
-};
+// function takeAction(container, action) {
+//     container.addEventListener(`click`, function(event){
+//         let itemId = event.target.id;
+//         if(itemId !== "items" && itemId !== "badge"){
+//             let item = items.filter(function(item){
+//                 return item.id == item.id;
+//             })[0];
+//             let itemInCart = cart.filter(function (item){
+//                 return item.id == itemId;
+//             })[0];
+//             if (itemInCart == undefined){
+//                 cart.push(item);
+//             } else if (action == "add"){
+//                 itemInCart.count++;
+//             } else if (action == "remove"){
+//                 itemInCart.count--;
+//             }
+//             console.log(cart);
+//             displayItems(cart, "cart");
+//         };
+//     });
+// };
+// }
+// }
 
-
-function displayItems(items, container){
-    let itemsContainer = getDiv(container);
-    itemsContainer.innerHTML = ``;
-    for(let i = 0; i < items.length; i++) {
-        let item = items[i];
-        let itemNode = createNote("li");
-        itemNode.setAttribute("id", item.id);
-        if(item.count > 0) {
-            itemNode.innerHTML = `${item.name} <span id="badge">${item.count}</span>`;
-            appendNode(itemsContainer, itemNode);
-        }
-    }
-}
-itemsContainer.innerHTML = '';
-displayItems(itemsArray, "items");
-
-
-function addToCart(action){
-    let container = '';
-    if(action == "add") {
-        container = getDiv("items");
-        takeAction(container)
-    } else if (action == "remove") {
-        container = getDiv("cart");
-        takeAction(container)
-    };
-}
-
-function takeAction(container, action) {
-    container.addEventListener(`click`, function(event){
-        let itemId = event.target.id;
-        if(itemId !== "items" && itemId !== "badge"){
-            let item = items.filter(function(item){
-                return item.id == item.id;
-            })[0];
-            let itemInCart = cart.filter(function (item){
-                return item.id == itemId;
-            })[0];
-            if (itemInCart == undefined){
-                cart.push(item);
-            } else if (action == "add"){
-                itemInCart.count++;
-            } else if (action == "remove"){
-                itemInCart.count--;
-            }
-            console.log(cart);
-            displayItems(cart, "cart");
-        };
-    });
-};
-
-addToCart('add');
-addToCart(`remove`);
+// addToCart('add');
+// addToCart(`remove`);
 // function addToCart(event) {
 
 
-    //// this took five hours to just restart and copy the example in class because that's all I seem to be able to get working. Google does not help.
-    // document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`, `<p>${items.price}</p>`);
-    // // document.getElementById(`shopping`).insertAdjacentHTML(`afterend`, `<button>Add to Cart</button>`);
-    // document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`, `<img src="${items.imageUrl}" alt="no picture fo you!">`);
-    // document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`,`<p>${items.description}</p>`);
-    // document.getElementById(`buy`).insertAdjacentHTML(`beforeend`, `<button>Add to Cart</button>`);
+//// this took five hours to just restart and copy the example in class because that's all I seem to be able to get working. Google does not help.
+
 
 // let clicks = document.querySelectorAll("shopping");
 // for (let i=0; i<clicks.length; i++){
@@ -83,36 +110,9 @@ addToCart(`remove`);
 // }
 
 // Cookies.set(`items`,item[0])
-let itemsArray = [{
-    1: { imageUrl: "https://cb.scene7.com/is/image/Crate/GoldWhiteOpticBallOrnSSF22?$web_pdp_main_carousel_thumb_med$", description: `White and Gold Optic Ball Ornament`, price: 5.0 },
-    2: {
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 9.0,
-    },
-    3: {
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 6.0,
-    },
-    4: {
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 8.0,
-    },
-    5: {
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 7.0,
-    },
-    6: {
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 10.0,
-    },
-}];
 
-let cart = [];
+
+// displayItems(itemsArray, "items");
 // let div = document.querySelector(`div`);
 // div.addEventListener(`click`, addToCart);
 // // console.log(document.querySelector(`div`));
