e34a873428666638d2073011c979253a9ee07a72
rk <rebeccakruk@outlook.com>
2022-11-21 19:03:40 -0700

    W11B More Complex Cookies complete

--- js/app.js
+++ js/app.js
@@ -1,124 +1,64 @@
-let itemsArray = [
-    {
-        id: 1,
-        imageUrl: "https://cb.scene7.com/is/image/Crate/GoldWhiteOpticBallOrnSSF22?$web_pdp_main_carousel_thumb_med$", 
-        description: `White and Gold Optic Ball Ornament`, 
-        price: 5.0
-    },
-    {
-        id: 2,
-        imageUrl: `https://cb.scene7.com/is/image/Crate/BoiledWoolOrnHolidaySlothSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Holiday Sloth Ornament`,
-        price: 9.0,
-    },
-    {
-        id: 3,
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 6.0,
-    },
-    {
-        id: 4,
-        imageUrl: `https://cb.scene7.com/is/image/Crate/SnowmanSnowglobeSSF20?$web_pdp_main_carousel_thumb_med$`,
-        description: `Snowman Tree Ornament`,
-        price: 8.0,
-    },
-    {
-        id: 5,
-        imageUrl: `https://cb.scene7.com/is/image/Crate/GlitterStarOrnamentsFSSF22/$web_pdp_main_carousel_med$/220727124059/glitter-star-ornaments.jpg`,
-        description: `Three Stars Ornaments, set of 3`,
-        price: 12.0,
-    },
-    {
-        id: 6,
-        imageUrl: `https://cb.scene7.com/is/image/Crate/NordicGnomeSSF20?$web_pdp_main_carousel_thumb_med$`,
-        description: `Gold Feather Ornament`,
-        price: 10.0,
-    },
-];
-
-console.log(itemsArray);
-itemsArray.map((item) => {
-    document.getElementById(`items`).insertAdjacentHTML(
-      `afterbegin`,
-      `<div id=${item.id}>
-        <h2>${item.description}</h2>
-        <img src=${item.imageUrl} alt=${item.description}>
-        <p class="price">${item.price}</p>
-        <button onClick='addToCart(${JSON.stringify(item)})'>Add To Cart</button>
-    </div>
-    `
-    );
-})
-
-let cart = [];
-
-
-function addToCart(item){
+function addToCart(item) {
     cart.push(item);
     console.log(cart);
     let cartJson = JSON.stringify(cart);
     Cookies.set(`addedToCart`, cartJson);
 }
 
-// function takeAction(container, action) {
-//     container.addEventListener(`click`, function(event){
-//         let itemId = event.target.id;
-//         if(itemId !== "items" && itemId !== "badge"){
-//             let item = items.filter(function(item){
-//                 return item.id == item.id;
-//             })[0];
-//             let itemInCart = cart.filter(function (item){
-//                 return item.id == itemId;
-//             })[0];
-//             if (itemInCart == undefined){
-//                 cart.push(item);
-//             } else if (action == "add"){
-//                 itemInCart.count++;
-//             } else if (action == "remove"){
-//                 itemInCart.count--;
-//             }
-//             console.log(cart);
-//             displayItems(cart, "cart");
-//         };
-//     });
-// };
-// }
-// }
+let itemsArray = [
+    {
+        id: 1,
+        imageUrl: "https://cb.scene7.com/is/image/Crate/GoldWhiteOpticBallOrnSSF22?$web_pdp_main_carousel_thumb_med$",
+        description: `White and Gold Optic Ball Ornament`,
+        price: `$5.00`,
+    },
+    {
+        id: 2,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/BoiledWoolOrnHolidaySlothSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Holiday Sloth Ornament`,
+        price: `$9.00`,
+    },
+    {
+        id: 3,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GoldenFeatherOrnamentSSF22?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: `$6.00`,
+    },
+    {
+        id: 4,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/SnowmanSnowglobeSSF20?$web_pdp_main_carousel_thumb_med$`,
+        description: `Snowman Tree Ornament`,
+        price: `$8.00`,
+    },
+    {
+        id: 5,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/GlitterStarOrnamentsFSSF22/$web_pdp_main_carousel_med$/220727124059/glitter-star-ornaments.jpg`,
+        description: `Three Stars Ornaments, set of 3`,
+        price: `$12.00`,
+    },
+    {
+        id: 6,
+        imageUrl: `https://cb.scene7.com/is/image/Crate/NordicGnomeSSF20?$web_pdp_main_carousel_thumb_med$`,
+        description: `Gold Feather Ornament`,
+        price: `$10.00`,
+    },
+];
 
-// addToCart('add');
-// addToCart(`remove`);
-// function addToCart(event) {
+// making the cart an array
+let cart = [];
 
-// let clicks = document.querySelectorAll("shopping");
-// for (let i=0; i<clicks.length; i++){
-//     clicks[i].addEventListener(`click`, addToCart)
-// }
-
-// Cookies.set(`items`,item[0])
-
-
-// displayItems(itemsArray, "items");
-// let div = document.querySelector(`div`);
-// div.addEventListener(`click`, addToCart);
-// // console.log(document.querySelector(`div`));
-
-// let cart = JSON.stringify(`items`);
-// Cookies.set(`items`, cart);
-// // console.log(ornament);
-
-// // Cookies.get(addToCart);
-// // console.log(addToCart);
-// // let addedToCart = Cookies.get(`addedToCart`)
-// // document.querySelector(`div`).addEventListener(`click`, addToCart);
-
-// let options = document.getElementById(`shopping`);
-// for (let key in items){
-//     options.insertAdjacentHTML(`afterbegin`, `<p>${items.description}</p>`);
-//     options.insertAdjacentHTML(`afterbegin`, `<p>${items.price}</p>`);
-//     options.insertAdjacentHTML(`afterend`, `<button>Add to Cart</button>`);
-//     options.insertAdjacentHTML(`afterbegin`, `<img src="${items.imageUrl}" alt="no picture fo you!">`);
-// console.log(`key:`, key);
-// }
+console.log(itemsArray);
+itemsArray.map((item) => {
+    document.getElementById(`items`).insertAdjacentHTML(
+        `afterbegin`,
+        `<div id=${item.id}>
+        <h3>${item.description}</h3>
+        <img src=${item.imageUrl} alt=${item.description}>
+        <p class="price">${item.price}</p>
+        <button onClick='addToCart(${JSON.stringify(item)})'>Add To Cart</button>
+    </div>
+    `
+    );
+});
 
 
--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,22 @@
 let cartItemsJson = Cookies.get(`addedToCart`);
-let cartItems = JSON.parse(cartItemsJson);
+let cartItems // placeholder. It won't work if I don't have this here
+
+// if cart is 'not' empty, it will parse the string from the previous 'home' page
+if (cartItemsJson !== undefined) {
+    cartItems = JSON.parse(cartItemsJson);
+}
 
 function removeFromCart(item, index) {
     console.log(`index`, index);
-    cartItems.splice(index, 1); // 2nd parameter means remove one item only
+    cartItems.splice(index, 1); // 2nd parameter means remove one item at a time
+    // the string is required here so all the information is included in the shopping cart
     let cartJson = JSON.stringify(cartItems);
     Cookies.set(`addedToCart`, cartJson);
     console.log(cartJson);
 }
-
-if (cartItems.length !== 0) {
+// the js has html built into it here, instead coded on the html page
+// map is working like a for loop, with the instructions following (ie. insert HTML)
+if (cartItems && cartItems.length !== 0) {
     cartItems.map((item, index) => {
         document.getElementById(`cart`).insertAdjacentHTML(
             `afterbegin`,
@@ -17,40 +24,13 @@ if (cartItems.length !== 0) {
         <h2>${item.description}</h2>
         <img src=${item.imageUrl} alt=${item.description}>
         <p class="price">${item.price}</p>
-        <button onClick='removeFromCart(${JSON.stringify(item, index)})'>remove From Cart</button>
+        <button onClick='removeFromCart(${JSON.stringify(item, index)})'>Remove From Cart</button>
     </div>
     `
         );
     });
 } else {
-          document.getElementById(`cart`).insertAdjacentHTML(
-            `afterbegin`,
-        `<h2>There are no items in the cart</h2>`)
-          }
-          console.log(`cartItemsJson`, cartItemsJson);
-
-
-// // Cookies.get(addedToCart);
-
-// let total = 0;
-// // addToCart(ornaments, ornaments)
-
-// for(let i = 0; i < item.length; i +- 1){
-//     total += (item[i].price * item[i].quantity);
-// }
-
-// console.log(total);
-//  document.body.insertAdjacentHTML(`afterbegin`, );
-//     document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`, `<p>${ornaments.price}</p>`);
-//     // document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`, `<img src="${ornaments.imageUrl}" alt="no picture fo you!">`);
-//     document.getElementById(`shopping`).insertAdjacentHTML(`afterbegin`,`<p>${ornaments.description}</p>`);
-//     document.getElementById(`shopping`).insertAdjacentHTML(`beforebegin`, `<button>Add to Cart</button>`);
-
-// addToCart(document.body.item);
-
-// function removeFromCart(){
-
-// }
-
-// document.getElementById(`removeFromCart`).addEventListener(`click`, removeFromCart)
-// let shop = document.getElementsByClassName(`buy`);
+    document.getElementById(`cart`).insertAdjacentHTML(
+    `afterbegin`,
+    `<h2>There are no items in the cart</h2>`)
+}
